1265cb9aefce044bfcb861b083c8a3d03e239307
xxx-gx1 <3081470459@qq.com>
2025-07-04 21:28:39 +0800

    智能前端之单词学习

---
+++ react/shotword/README.md
@@ -0,0 +1,23 @@
+# AI 单词拍照移动应用
+
+- mobile App
+- css reset
+- 组件划分思维
+  - 功能逻辑划分 图片上传
+  - App 根组件
+  - PictureCard 子组件
+  - 组件通信
+    - 父组件负责持有状态
+    - 父组件api 请求
+    - 子组件消费数据
+    - state(私有数据状态)、props(父组件传递的数据状态) 都是数据
+    - 子组件修改状态，通过回调函数通知父组件，父组件修改状态
+- 目录项目
+  - src/ 开发目录
+  - components/ 组件目录
+    - 组件就是一个文件夹
+      - jsx
+      - css
+  - public/ 静态资源目录
+  - libs/ 工具包
+  - .env.local  ai api-key token 等环境变量
---
+++ react/shotword/src/components/PictureCard/index.jsx
@@ -0,0 +1,44 @@
+import './style.css'
+import { useState } from 'react'
+
+
+const PictureCard = (props) => {
+    const {
+        word,
+        audio,
+        uploadImg
+    } = props
+    const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
+    const uploadImgData = (e) => {
+        const file = (e.target).files?.[0];
+        if (!file) { return; }
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const data = reader.result;
+                setImgPreview(data);
+                uploadImg(data);
+                resolve(data);
+            }
+            reader.onerror = (error) => { reject(error); };
+        })
+    }
+
+    return (
+        <div className="card">
+            <input 
+            type="file" 
+            id="selectImage"
+            accept='.jpg,.jpeg,.png,.gif'
+            onChange={uploadImgData}
+            />
+            <label htmlFor="selectImage" className='upload'>
+                <img src={imgPreview} alt="preview" />
+            </label>
+            <div className="word">{word}</div>
+        </div>
+    )
+}
+
+export default PictureCard
