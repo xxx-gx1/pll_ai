94bc5ab30359469d5414a413e225c27b86d3fc2f
xxx-gx1 <3081470459@qq.com>
2025-07-31 20:59:19 +0800

    移动端适配和路由等功能

---
+++ Nebula Rest/src/App.jsx
@@ -0,0 +1,45 @@
+import { 
+  Suspense,
+  lazy
+} from 'react'
+import {
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom'
+import './App.css'
+const Home = lazy(() => import('@/pages/Home'))
+const Record = lazy(() => import('@/pages/Record'))
+const Stories = lazy(() => import('@/pages/Stories'))
+const Profile = lazy(() => import('@/pages/Profile'))
+import MainLayout from '@/components/MainLayout'
+import BlankLayout from '@/components/BlankLayout'
+import Loading from '@/components/Loading'
+
+function App() {
+
+  return (
+    <>
+    <Suspense fallback={<Loading />}>
+        {/* 带有tabbar的Layout */}
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path='/' element={<Navigate to="/home" />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/record' element={<Record />} />
+            <Route path='/stories' element={<Stories />} />
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+        </Routes>
+        {/* 空Layout */}
+        <Routes>
+          <Route element={<BlankLayout />}>
+            {/* <Route path='/search' element={<Search />} /> */}
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
---
+++ Nebula Rest/src/components/MainLayout/index.jsx
@@ -0,0 +1,62 @@
+import {
+    useState,
+    useEffect
+} from 'react';
+import {
+    Tabbar
+} from 'react-vant';
+import {
+    HomeO,
+    UserO,
+    NotesO,
+} from '@react-vant/icons';
+import { 
+    Outlet,
+    useNavigate,
+    useLocation
+} from 'react-router-dom'
+
+// 菜单栏配置
+const tabs = [
+    {title: '入睡',icon: <HomeO/>,path: '/home'},
+    {title: '记录',icon: <NotesO />,path: '/record'},
+    {title: '故事',icon: <HomeO/>,path: '/stories'},
+    {title: '我的',icon:  <UserO />,path: '/profile'},
+
+]
+const MainLayout = () => {
+    const [active, setActive] = useState(0);
+    const navigate = useNavigate();
+    const location = useLocation();
+    useEffect(() => {
+        console.log(location.pathname,'/////');
+        // es6的使用power
+        const index = tabs.findIndex(tab => location.pathname.startsWith(tab.path));
+        setActive(index);
+    },[])
+  return (
+    <div 
+    className='flex flex-col h-screen'
+    style={{paddingBottom: '60px'}}
+    >
+      <div className='flex-1'>
+        <Outlet />
+      </div>
+      {/* tabbar */}
+      <Tabbar value={active} onChange={
+        (key) => {
+            setActive(key);
+            navigate(tabs[key].path);
+        }
+      }>
+        {tabs.map((item, index) => (
+            <Tabbar.Item key={index} icon={item.icon}>
+                {item.title}
+            </Tabbar.Item>
+        ))}
+      </Tabbar>
+    </div>
+  )
+}
+
+export default MainLayout
