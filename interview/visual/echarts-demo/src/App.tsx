bc5801f477878e35e0e5e4fd03a9fb823df47625
xxx-gx1 <3081470459@qq.com>
2025-08-25 21:35:00 +0800

    feat: 初始化echarts可视化项目基础结构
    添加Vite+React+TS项目基础配置
    包含echarts图表组件实现
    配置TypeScript和ESLint支持

---
+++ interview/visual/echarts-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Demo from '../components/Demo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Demo />
+    </>
+  )
+}
+
+export default App
